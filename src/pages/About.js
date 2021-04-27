e35937c44a4f1a2722ca4b4b9a2d6faa329a4b28
Danny Gavin <homevpn14@gmail.com>
2021-04-27 15:58:35 -0700

    Style adjustments

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const About = () => {
-    return <h1>This is the about page</h1>
+    return <h1 className="about">This is an app that shows you the current stock prices for multiple tech companies. It also gives you a description of each company!</h1>
 }
 
 export default About
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Home = () => {
-    return <h1>This is the home page</h1>
+    return <h1>Welcome to the home page!</h1>
 }
 
 export default Home
